d4b808fb7353003f70eebe63da0cd0bdd8ebea41
Lucas Rezende <73916809+lksrnd@users.noreply.github.com>
2023-05-17 20:59:25 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from socket  import *
 from constCS import * #-
+import pickle
 
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
@@ -9,5 +10,11 @@ while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if not data: break       # stop if client stopped
   print(bytes.decode(data))
+ if data[0] == 'add':
+    result = data[1][0] + data[1][1]
+     
+elif data[0] == 'subtract':
+    result = data[1[0] - data[1][0]
+  
   conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
 conn.close()               # close the connection
