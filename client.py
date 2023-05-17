a7b602415d11d014146f7922ff1e2ace8cba77f8
Caio Oliveira <73246374+caioOlivera@users.noreply.github.com>
2023-05-17 20:59:25 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,10 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+
+operation1 = ('subtract',(10,20))
+
+s.send(pickle.dumps(operation1))  # send some data
 data = s.recv(1024)     # receive the response
 print (bytes.decode(data))            # print the result
 s.close()               # close the connection
